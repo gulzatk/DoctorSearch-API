4e8fbd88f8fb89c7097aea1ffc0ba1f27b5ecd6c
Gulzat Karimova <gulzat.karimova@gmail.com>
2019-01-18 15:02:01 -0800

    It will update the file to add backend

--- src/doctor.js
+++ src/doctor.js
@@ -13,40 +13,22 @@ export class Doctor {
           request.open("GET", url, true);
           request.send();
         });
+      }
+
+        getDoctorByIssue(issue) {
+            return new Promise(function(resolve, reject) {
+              let request = new XMLHttpRequest();
+              let url = `https://api.betterdoctor.com/2016-03-01/doctors?&query=${issue}&location=wa-seattle&skip=0&limit=15&user_key=${process.env.API_KEY}`;
+              request.onload = function() {
+                if (this.status === 200) {
+                  resolve(request.response);
+                } else {
+                  reject(Error(request.statusText));
+                }
+              }
+              request.open("GET", url, true);
+              request.send();
+            });
     }
 
-    // getRandomArbitrary(num) {
-    //     return Math.floor(Math.random() * (num - 1) + 1);
-    // }
-    //
-    // getPokemonByType(type) {
-    //     return new Promise(function(resolve, reject) {
-    //       let request = new XMLHttpRequest();
-    //       let url = `https://pokeapi.co/api/v2/type/${type}`;
-    //       request.onload = function() {
-    //         if (this.status === 200) {
-    //           resolve(request.response);
-    //         } else {
-    //           reject(Error(request.statusText));
-    //         }
-    //       }
-    //       request.open("GET", url, true);
-    //       request.send();
-    //     });
-    // }
-    //
-    // getRandomPokemonGif() {
-    //     return new Promise(function(resolve, reject) {
-    //         let request = new XMLHttpRequest();
-    //         let url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=pokemon&rating=G`;
-    //         request.onload = function() {
-    //             if (this.status === 200) {
-    //                 resolve(request.response);
-    //             } else {
-    //                 reject(Error(request.statusText));
-    //             }
-    //         }
-    //         request.open("GET", url, true);
-    //         request.send();
-    //     });
     }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Doctor } from './doctor.js';
 
 $(document).ready(function() {
+
   $("#searchByName").submit(function(event) {
     event.preventDefault();
     let name = $("#doctorName").val();
@@ -15,10 +16,30 @@ $(document).ready(function() {
       let body = JSON.parse(response);
       let doctorList = body.data;
       if (doctorList.length === 0) {
-        $("#doctorOutput").text("Doctor not found");
+        $("#doctorOutput").append("Doctor not found");
       }else {
-        ("#doctorOutput").text(name.charAt(0).toUpperCase() + name.slice(1));
+        for (let i = 0; i<doctorList.length; i++) {
+          $("#doctorOutput").append("<h3>" + doctorList[i].profile.first_name + " " + doctorList[i].profile.last_name + "." + "</h4");
+        }
+      }
+    });
+  });
 
+  $("#searchBySymptoms").submit(function(event) {
+    event.preventDefault();
+    let issue = $("#issue").val();
+    let doc = new Doctor();
+    let promise = doc.getDoctorByIssue(issue);
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      let result = body.data;
+      if (result.length === 0) {
+        $("#doctorOutput").append("Doctor not found");
+      }else {
+        for (let i = 0; i<result.length; i++) {
+          $("#doctorOutput").append("<h3>" + result[i].profile.first_name + " " + result[i].profile.last_name + "." + "</h4");
+        }
       }
     });
   });

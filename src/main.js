ab2c00891b8c18c8b87d100878c396fbdfbb89d2
Gulzat Karimova <gulzat.karimova@gmail.com>
2019-01-18 12:05:12 -0800

    It will commit the file to add a js file using API, promise

--- src/main.js
+++ src/main.js
@@ -2,8 +2,24 @@ import './css/styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Pokemon } from './js/pokemon';
+import { Doctor } from './doctor.js';
 
-$(document).ready(function () {
+$(document).ready(function() {
+  $("#searchByName").submit(function(event) {
+    event.preventDefault();
+    let name = $("#doctorName").val();
+    let newDoctor = new Doctor();
+    let promise = newDoctor.getDoctorByName(name);
 
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      let doctorList = body.data;
+      if (doctorList.length === 0) {
+        $("#doctorOutput").text("Doctor not found");
+      }else {
+        ("#doctorOutput").text(name.charAt(0).toUpperCase() + name.slice(1));
+
+      }
+    });
+  });
 });

a6660c736abdf7e939a24c4c15ab62c239bb4481
Gulzat Karimova <gulzat.karimova@gmail.com>
2019-01-18 17:04:17 -0800

    It will update the file to add more functionality

--- src/doctor.js
+++ src/doctor.js
@@ -30,37 +30,37 @@ export class Doctor {
       request.send();
     });
   }
-
-  getBySpecialty() {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/specialties?skip=0&limit=20&user_key=${process.env.API_KEY}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-  }
-
-  getAllDoctors() {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?&location=wa-seattle&skip=0&limit=15&user_key=${process.env.API_KEY}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-  }
+  // 
+  // getBySpecialty() {
+  //   return new Promise(function(resolve, reject) {
+  //     let request = new XMLHttpRequest();
+  //     let url = `https://api.betterdoctor.com/2016-03-01/specialties?skip=0&limit=20&user_key=${process.env.API_KEY}`;
+  //     request.onload = function() {
+  //       if (this.status === 200) {
+  //         resolve(request.response);
+  //       } else {
+  //         reject(Error(request.statusText));
+  //       }
+  //     }
+  //     request.open("GET", url, true);
+  //     request.send();
+  //   });
+  // }
+  //
+  // getAllDoctors() {
+  //   return new Promise(function(resolve, reject) {
+  //     let request = new XMLHttpRequest();
+  //     let url = `https://api.betterdoctor.com/2016-03-01/doctors?&location=wa-seattle&skip=0&limit=15&user_key=${process.env.API_KEY}`;
+  //     request.onload = function() {
+  //       if (this.status === 200) {
+  //         resolve(request.response);
+  //       } else {
+  //         reject(Error(request.statusText));
+  //       }
+  //     }
+  //     request.open("GET", url, true);
+  //     request.send();
+  //   });
+  // }
 
 }
--- src/main.js
+++ src/main.js
@@ -4,19 +4,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Doctor } from './doctor.js';
 
-let doctor = new Doctor();
-let promise = doctor.getBySpecialty();
-
-promise.then(function(response) {
-    let body = JSON.parse(response);
-    let list = body.data;
-    const selectForm = $("#specialty");
-    console.log(selectForm);
-    console.log(list);
-    for(let i = 0; i < list.length; i++){
-        selectForm.append(`<option value = "${list[i].name}">${list[i].name}</option>`);
-    }
-});
+// let doctor = new Doctor();
+// let promise = doctor.getBySpecialty();
+//
+// promise.then(function(response) {
+//     let body = JSON.parse(response);
+//     let list = body.data;
+//     const selectForm = $("#specialty");
+//     console.log(selectForm);
+//     console.log(list);
+//     for(let i = 0; i < list.length; i++){
+//         selectForm.append(`<option value = "${list[i].name}">${list[i].name}</option>`);
+//     }
+// });
 
 $(document).ready(function() {
 
@@ -33,8 +33,10 @@ $(document).ready(function() {
         $("#doctorOutput").append("Doctor not found");
       }else {
         for (let i = 0; i<doctorList.length; i++) {
-          $("#doctorOutput").append("<h3>" + doctorList[i].profile.first_name + " " + doctorList[i].profile.last_name + "." + "</h4");
-        }
+          $("#doctorOutput").append("<h3>" + doctorList[i].profile.first_name + " " + doctorList[i].profile.last_name + "." + "</h3"> + "Phone number: " + doctorList[i].practices[0].phones[0].number + "<br>" +"<h4>" +  "Address: " + "</h4>" + "City: " + doctorList[i].practices[0].visit_address.city + "State" + doctorList[i].practices[0].phones[0].visit_address.state +
+          "Street: " + doctorList[i].practices[0].visit_address.street + "<br>" +
+          "Zip: " + doctorList[i].practices[0].visit_address.zip + "<br>");
+    }
       }
     });
   });
@@ -52,7 +54,9 @@ $(document).ready(function() {
         $("#doctorOutput").append("Doctor not found");
       }else {
         for (let i = 0; i<result.length; i++) {
-          $("#doctorOutput").append("<h3>" + result[i].profile.first_name + " " + result[i].profile.last_name + "." + "</h4");
+          $("#doctorOutput").append("<h3>" + result[i].profile.first_name + " " + result[i].profile.last_name + "." + "</h3"> + "Phone number: " + result[i].practices[0].phones[0].number + "<br>" +"<h4>" +  "Address: " + "</h4>" + "City: " + result[i].practices[0].visit_address.city + "State" + result[i].practices[0].phones[0].visit_address.state +
+          "Street: " + result[i].practices[0].visit_address.street + "<br>" +
+          "Zip: " + result[i].practices[0].visit_address.zip + "<br>" );
         }
       }
     });
